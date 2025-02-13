472be6264345e649a78f0c84873d4cf8f352656e
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-02-13 12:04:45 -0500

    fix: changing from css to tailwindcss
    choosing css was a big mistake so it is better to change direction
    before the project grows, why it was a mistake well i was facing the
    number one difficult problem which is naming 😊

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,10 +9,10 @@ onMounted(() => {
 	window.addEventListener("scroll", (e) => {
 		e.preventDefault()
 		if (window.scrollY > 70) {
-			wrapper.value?.classList.add("onScroll")
+			wrapper.value?.classList.add("")
 		}
 		else {
-			wrapper.value?.classList.remove("onScroll");
+			wrapper.value?.classList.remove("");
 
 		}
 		console.log(wrapper.value.classList)
@@ -21,94 +21,21 @@ onMounted(() => {
 })
 </script>
 <template>
-	<div ref="wrapper" class="wrapper">
-		<div class="content">
-			<div class="logo">Drivee</div>
-			<div class="interactions">
+	<div ref="wrapper" class="w-full mx-auto">
+		<div class="flex justify-between ">
+			<div class="">Drivee</div>
+			<div class="">
 
-				<div class="navigations">
-					<div class="pages" v-for="i in pages" :key="i">{{ i }}</div>
+				<div class="">
+					<div class="" v-for="i in pages" :key="i">{{ i }}</div>
 				</div>
 
-				<div class="buttons">
-					<div class="button">Sign In</div>
-					<div class="button">Sign Up</div>
+				<div class="">
+					<div class="">Sign In</div>
+					<div class="">Sign Up</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
-<style scoped>
-.wrapper {
-	height: 6rem;
-	height: 7rem;
-	/* background: rgba(255, 255, 255, 0.2); */
-	border-bottom: 0.1px solid var(--primary-gray);
-	display: flex;
-	align-items: center;
-	backdrop-filter: blur(2px);
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-	transition: background-color 0.8s, height 0.8s;
-}
-
-.onScroll {
-	background-color: var(--primary-white) !important;
-	border: transparent;
-	height: 5rem;
-}
-
-
-.onScroll .logo,
-.onScroll .navigations,
-.onScroll .button {
-	color: var(--primary-black);
-}
-
-.interactions {
-	display: flex;
-	width: 75%;
-	justify-content: space-around;
-}
-
-.content {
-	width: 75%;
-	margin-inline: auto;
-	display: flex;
-	justify-content: space-between;
-	font-weight: 500;
-}
-
-.navigations {
-	display: flex;
-	justify-content: space-evenly;
-	align-items: center;
-	min-width: 40%;
-	cursor: pointer;
-}
-
-.logo {
-	font-size: 24px;
-	cursor: pointer;
-	display: flex;
-	align-items: center;
-	text-align: center;
-	color: var(--primary-black);
-}
-
-.buttons {
-	display: flex;
-	gap: 2rem;
-}
-
-.button {
-	padding: 14px;
-	border: 1px solid #000;
-	border-radius: 8px;
-	cursor: pointer;
-	background-color: var(--primary-black);
-	color: var(--primary-white) !important;
-}
-</style>
+<style scoped></style>
---
+++ src/components/ShowCase.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const buttons: Array<string> = ["Rent Car", "Rent Bike"]
+</script>
+<template>
+	<div class="">
+		<div class="">
+			<div class="">
+				<div class="">
+					Unlock Endless Driving With Drivee
+				</div>
+
+				<div class="">
+					To contribute to positive change and achieve our sustainability goals with many
+					extraordinary
+				</div>
+				<div class="">
+					<div class="" v-for="b in buttons" :key="b"> {{ b }}</div>
+				</div>
+			</div>
+			<div class="">
+				<div class="">
+					Unlock Endless Driving With Drivee
+				</div>
+
+				<div class="">
+					To contribute to positive change and achieve our sustainability goals with many
+					extraordinary
+				</div>
+				<div class="">
+					<div class="" v-for="b in buttons" :key="b"> {{ b }}</div>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+</template>
+<style scoped></style>
