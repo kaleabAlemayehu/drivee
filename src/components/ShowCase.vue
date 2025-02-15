8c7dc40107050b045385bbce49e544a301bd6e2d
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-02-15 03:30:41 -0500

    feat: adding floating sorter panel on the showcase

--- src/components/ShowCase.vue
+++ src/components/ShowCase.vue
@@ -1,30 +1,54 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 <template>
-	<div class="bg-black w-screen h-screen flex items-center relative overflow-x-hidden">
-		<div class="flex mx-auto min-w-5/6 px-10 ">
-			
-			<div class="">
+  <div
+    class="bg-black -z-10 w-full h-screen flex items-center relative overflow-hidden"
+  >
+    <div class="flex mx-auto min-w-5/6 px-10">
+      <div class="">
+        <div class="text-7xl w-3/5 leading-22 font-semibold my-12">
+          Unlock Endless Driving With Drivee
+        </div>
 
-			<div class="text-7xl w-3/5 leading-22 font-semibold my-12">
-					Unlock Endless Driving With Drivee
-			</div>
-
-			<div class=" text-xl font-light w-2/5">
-				To contribute to positive change and achieve our sustainability goals with many extraordinary
-			</div>
-			<div class="my-16 mx-0 flex justify-around w-1/4">
-				<div class=" rounded-md border-white border-[0.1px] outline-none ring-0 py-3 px-6 text-black bg-white! cursor-pointer">Rent Car</div>
-				<div class=" rounded-md border-white border-[0.1px] outline-none ring-0 py-3 px-6 text-white! bg-black cursor-pointer">Rent Bike</div>
-			</div>
-			<div class="">
-				<img src="./../assets/map.svg" alt="" class="absolute top-50 -right-40 z-10">
-				<img src="./../assets/bmw.png" alt="" class="scale-200 absolute top-80 right-70 z-20">
-				<img src="./../assets/moto.png" alt="" class="absolute top-90 -right-25 z-30 scale-85">
-			</div>
-			</div>
-		</div>
-	</div>
+        <div class="text-xl font-light w-2/5">
+          To contribute to positive change and achieve our sustainability goals
+          with many extraordinary
+        </div>
+        <div class="my-16 mx-0 flex justify-around w-1/4">
+          <div
+            class="rounded-md border-white border-[0.1px] outline-none ring-0 py-3 px-6 text-black bg-white! cursor-pointer"
+          >
+            Rent Car
+          </div>
+          <div
+            class="rounded-md border-white border-[0.1px] outline-none ring-0 py-3 px-6 text-white! bg-black cursor-pointer"
+          >
+            Rent Bike
+          </div>
+        </div>
+        <div class="">
+          <img
+            src="./../assets/map.svg"
+            alt=""
+            class="absolute top-50 -right-40 z-10 m-0"
+          />
+          <img
+            src="./../assets/bmw.png"
+            alt=""
+            class="scale-200 absolute top-80 right-70 z-20 m-0"
+          />
+          <img
+            src="./../assets/moto.png"
+            alt=""
+            class="absolute top-90 -right-25 z-30 scale-85 m-0"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-white m-0 p-0">
+    <div
+      class="absolute bg-white h-48 rounded-lg shadow-gray-200 shadow-lg w-5/6 -bottom-25 z-50! left-0 right-0 mx-auto"
+    ></div>
+  </div>
 </template>
 <style scoped></style>
