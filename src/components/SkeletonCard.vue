880e47961fb31f47dea183a5932e14f06528db5e
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-02-19 18:12:36 -0500

    feat: adding skeleton card for loading state

---
+++ src/components/SkeletonCard.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    class="w-[24rem] py-2 px-6 h-[34rem] bg-white rounded-xl flex flex-col border-gray-300 border-[1.2px]"
+  >
+    <div
+      class="rounded-lg animate-pulse bg-gray-200 w-95/100 h-45/100 my-4 mx-auto"
+    ></div>
+    <div class="flex justify-between my-2">
+      <div class="flex-1 flex flex-col">
+        <div
+          class="w-70/100 h-5 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+        ></div>
+        <div
+          class="w-90/100 h-5 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+        ></div>
+      </div>
+
+      <div
+        class="w-40/100 h-12 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+      ></div>
+    </div>
+    <div class="flex justify-between my-2">
+      <div class="flex-1 flex flex-col">
+        <div
+          class="w-70/100 h-5 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+        ></div>
+        <div
+          class="w-90/100 h-5 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+        ></div>
+      </div>
+      <div class="flex-1 flex items-end justify-end flex-col">
+        <div
+          class="w-90/100 h-5 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+        ></div>
+        <div
+          class="w-70/100 h-5 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+        ></div>
+      </div>
+    </div>
+    <div class="flex justify-between my-2">
+      <div class="flex-1 flex flex-col">
+        <div
+          class="w-90/100 h-5 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+        ></div>
+        <div
+          class="w-70/100 h-5 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+        ></div>
+      </div>
+      <div
+        class="w-40/100 h-12 bg-gray-200 ml-2 mb-2 rounded-xl animate-pulse"
+      ></div>
+    </div>
+  </div>
+</template>
+<style scoped></style>
