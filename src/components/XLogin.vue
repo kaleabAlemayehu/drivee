e98cecdf4f4d6088b5382cb22f94740246f50aa0
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-06-14 19:08:43 -0400

    feat: adding fixed size to make similar with google button and getting pkce from the server

--- src/components/XLogin.vue
+++ src/components/XLogin.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue';
+import { useClient } from '../composables/useClient';
+import type { AxiosError } from 'axios';
+const { get } = useClient();
+const handleClick = async () => {
+  try {
+    const res = await get('/auth/x/pkce');
+    console.log(res.data);
+  } catch (error: any | AxiosError) {
+    console.log(error);
+  }
+};
 </script>
 <template>
   <div
-    class="w-78/100 bg-black text-white rounded-full px-1 py-[0.6rem] cursor-pointer select-none hover:bg-stone-700 transition-colors flex justify-center items-center"
+    @click="handleClick"
+    class="w-[400px] bg-black text-white rounded-full px-1 py-[0.6rem] cursor-pointer select-none hover:bg-stone-700 transition-colors flex justify-center items-center"
   >
     <div class="w-10 mr-12"></div>
     <div class="text-sm font-normal text-white">Continue with</div>
